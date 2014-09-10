fd8a968e8859309f81a7fb63a84792790ecebe30
Shana Joseph <sj250040@muhlenberg.edu>
2014-09-10 16:25:19 -0400

    bin2dec

--- bin2dec.py
+++ bin2dec.py
@@ -1,3 +1,9 @@
 # this program converts binary to decimal numbers 
-  def bin2dec():
+def bin2dec(num): 
+  result = 0
+  for index in range(len(num)):
+    digit = num[index] 
+    if digit == "1":
+      result = result + pow(2,len (num)-index-1) 
+  return result
   
